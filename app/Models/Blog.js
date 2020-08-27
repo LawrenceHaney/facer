1b38b3a0c9ca2cfacbf4459864780820cb8ad960
AddysonH <iamaddyson@gmail.com>
2020-08-27 16:56:37 -0600

    functional

--- app/Models/Blog.js
+++ app/Models/Blog.js
@@ -1,6 +1,6 @@
 import { generateId } from "../uits.js"
 export default class Blog {
-    constructor({title, body, id}) {
+    constructor({ title, body, id }) {
         this.title = title
         this.body = body || ""
         this.id = id || generateId()
@@ -8,27 +8,37 @@ export default class Blog {
     }
 
     get Template() {
-        return `<h1 class="col-12">${this.title}</h1> 
-        <p class="col-12">${this.body}</p>
-        <div class col-12>${this.commits} </div>
-        <div class col-12>${this.penutgallery} </div>
-        <button type="button" class="btn btn-danger" onclick="app.blogsController.deleteBlog('${this.id}')"> bye</button>`
+        return `
+        
+        <div class="row card bg-success d-flex p-2 m-3">
+            <i class="fa fa-ban text-danger align-self-end" aria-hidden="true"
+                onclick="app.blogsController.deleteBlog('${this.id}')"></i>
+            <div class="card bg-secondary m-2">
+                <h1 class="col-6">${this.title}</h1>
+                <p class="col-12">${this.body}</p>
+            </div>
+            <div class="d-flex col-12 p-2">${this.commits} </div>
+            <div class="col-12">${this.penutgallery} </div>
+        </div>`
     }
 
-    get penutgallery(){
+    get penutgallery() {
         return ` <form onsubmit="app.blogsController.newPenut(event, '${this.id}')">
         <div class="form-group" >
             <label for="">Your advice </label>
             <input type="text" class="form-control" name="penut" id="penut${this.id}" aria-describedby="helpId" placeholder="some text to fill the space">
-            <button type="submit" class="btn btn-primary">post forever <i class="fa fa-heart" aria-hidden="true"></i></button>
+            <button type="submit" class="btn btn-primary m-2">post forever <i class="fa fa-heart" aria-hidden="true"></i></button>
         </div>
     </form> `
     }
-    get commits(){
-        let template= ''
+    get commits() {
+        let template = ''
         this.penut.forEach(e => {
-            template += `<h4> ${e} </h4>
-            <button type="button" class="btn btn-danger" onclick="app.blogsController.deletePenut('${this.id}', '${e}')"> bye</button>`
+            template += `
+            <i class="fa fa-ban text-danger justify-self-right " aria-hidden="true"
+            onclick="app.blogsController.deletePenut('${this.id}', '${e}')"></i>
+            <h4> ${e} </h4>
+            <hr>`
         });
         return template
     }
